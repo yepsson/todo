af649a4e572cd3593d156dd147fdd888328bd53c
yepsson <32198001+yepsson@users.noreply.github.com>
2017-11-15 20:05:17 +0100

    grundkod
    grundkod

---
+++ todo2/script.js
@@ -0,0 +1,116 @@
+
+sessionStorage.userName = "test";
+sessionStorage.passWord = "password";
+
+$(document).ready(function () {
+
+    status();
+
+    
+    //login function in navbar
+    $(".login").click(function(){
+
+        if ($(".user").val() == sessionStorage.userName && $(".pass").val() == sessionStorage.passWord) {
+
+            sessionStorage.loggedin = "true";
+            status();
+
+        }
+        else {
+
+            wrong();
+
+        }
+    });
+
+    //function that calls if user + pass is right
+    function right() {
+
+        
+
+        var text = $("<h1></h1>").text("Välkommen");
+
+        $("#form").hide();
+        $("#login").append("<button class='logout'>Logga ut</button>");
+        $("#main").empty();
+        $("#main").append(text);
+        
+
+        //logout function
+        $(".logout").click(function(){
+            sessionStorage.loggedin = "false";
+            $("#form").show();
+            $(".logout").hide();
+            startpage();
+        });
+
+
+        list();
+
+    }
+
+    //function that calls if user or pass is wrong
+    function wrong() {
+
+        alert("Fel användarnamn eller Lösenord! ")
+        $("#main").empty();
+        $("#main").append("<p>Glömt lösenord eller användarnamn?!</p><a href='#'>Klicka här!</a>");
+    }
+
+    //checks status if logged in or not
+    function status() {
+
+         if (sessionStorage.loggedin == "true") {
+
+            right();
+        }
+
+        else {
+
+            startpage();
+        }
+    }
+
+    //startpage
+    function startpage() {
+
+        $("#main").empty();
+        $("#main").append("<h2>Hej och välkommen</h2>");
+        $("#main").append("<p>Detta är en Todo sida, för att ta del av listan måste du logga in.</p>");
+
+    }
+
+    //function to loop the todo-list
+    function list() {
+
+        $("#main").append("<h4>Att göra:</h4>");
+        $("#main").append("<ul class='stuffList'></ul>");
+
+        var stuffToDo = ["Klipp gräset",
+            "Betala räkningar",
+            "Köp mjölk",
+            "Spika upp tavlor",
+            "Rasta hundarna",
+            "Ge hundarna mat",];
+
+        localStorage.setItem("stuffToDo", JSON.stringify(stuffToDo));
+        stuffToDo = JSON.parse(localStorage.getItem("stuffToDo"));
+        
+        $.each(stuffToDo, function(value, index) {
+            $(".stuffList").append("<li>" + index + "</li>");
+          });
+
+    }
+    //gets actuall date and time
+    document.getElementById("date").innerHTML = Date();
+
+});
+
+
+
+
+
+
+
+
+
